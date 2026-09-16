28878f97491e8cbc299e0de1607214cb7ae505ae
Michel Fodje <michel4j@gmail.com>
2026-09-16 16:48:31 -0600

    style: Refactor card layouts and adjust separator visibility across multiple templates

--- basiclive/core/lims/templatetags/bl_icons.py
+++ basiclive/core/lims/templatetags/bl_icons.py
@@ -4,7 +4,7 @@ register = Library()
 
 
 @register.inclusion_tag('lims/components/icon-info.html')
-def show_icon(label='', icon='', badge=None, color='', tooltip='', show_null=False):
+def show_icon(label='', icon='', badge=None, color=None, tooltip='', show_null=False):
     badge = None if not badge and not show_null else badge
     return {
         'label': label,
